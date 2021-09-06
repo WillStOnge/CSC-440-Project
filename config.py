c99e011740be090f1ef2e2dd72a6cd0e75720eb2
Will St. Onge <ongewill@gmail.com>
2021-09-06 09:43:40 -0400

    Initialize

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = 'C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\Stage A\\content'
+USER_DIR = 'C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\Stage A\\user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
