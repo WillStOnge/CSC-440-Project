420e5d5b60616485595152bfdb6a4067502f1db7
Will St. Onge <ongewill@gmail.com>
2021-11-13 15:12:50 -0500

    Updated config and renamed Riki file.

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-# encoding: utf-8
-
-SECRET_KEY='a unique and long key'
+SECRET_KEY= "a unique and long key"
 TITLE = "PyKi"
 HISTORY_SHOW_MAX = 30
 CONTENT_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\content"
-USER_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\user"
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
 CONNECTION_STRING = "DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE=PyKi.db;Trusted_connection=yes"
