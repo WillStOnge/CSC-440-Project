37a21d2589bc23754e3f359bc5050669c983c450
Will St. Onge <ongewill@gmail.com>
2021-11-13 14:10:15 -0500

    Added role_id to role class

--- wiki/web/role.py
+++ wiki/web/role.py
@@ -3,21 +3,27 @@ from wiki.web.role_manager import RoleManager
 from wiki.web.database import Database
 
 class Role:
-    _name: str
-
-    def __init__(self, role_name: str):
-        _name = role_name
-
-    @property
-    def name(self):
-        return _name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            raise TypeError('Incompatible type, excepted string got ', type(value))
-        _name = value
+    def __init__(self, role_id: int, role_name: str):
+        self._role_id = role_id
+        self._role_name = role_name
 
 
     def get_manager() -> RoleManager:
         return RoleManager(Database())
+
+
+    @property
+    def role_id(self):
+        return self._role_id
+
+
+    @property
+    def role_name(self):
+        return self._role_name
+
+
+    @role_name.setter
+    def role_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Incompatible type, excepted string got ', type(value))
+        self._role_name = value
