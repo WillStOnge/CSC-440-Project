0ecfcffde8ff3a305deb9feb738565cb0c2fbb8f
Anthony Bosch <boscha1@mymail.nku.edu>
2021-11-20 11:13:36 -0500

    Revert "fixed untracked files"
    This reverts commit 1c6948e6e492151297a77b5cf80e4e50cd84788b.

---
+++ config.py
@@ -0,0 +1,7 @@
+SECRET_KEY= "a unique and long key"
+TITLE = "PyKi"
+HISTORY_SHOW_MAX = 30
+CONTENT_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\content"
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
+CONNECTION_STRING = "DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE=PyKi.db;Trusted_connection=yes"
