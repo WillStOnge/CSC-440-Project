febf6f47de9d428f3e5b8f8cb8ece60a9ab7dc63
Will St. Onge <ongewill@gmail.com>
2021-11-27 22:52:29 -0500

    Integrated upload feature into the wiki.

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 SECRET_KEY= "a unique and long key"
 TITLE = "PyKi"
-HISTORY_SHOW_MAX = 30
-CONTENT_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\content"
-NUMBER_OF_HISTORY = 5
 PRIVATE = True
+CONTENT_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\content"
+UPLOAD_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\upload"
+HISTORY_SHOW_MAX = 30
+NUMBER_OF_HISTORY = 5
+FILE_SIZE_MAX_MB = 16
 CONNECTION_STRING = "DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE=PyKi.db;Trusted_connection=yes"
