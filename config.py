ff515bacfc3187c64be4142e4d4fa82774657e5f
Will St. Onge <ongewill@gmail.com>
2021-11-30 23:57:16 -0500

    Updated config file

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-SECRET_KEY= "a unique and long key"
 TITLE = "PyKi"
 PRIVATE = True
-CONTENT_DIR = "C:\\Users\\Justin Gallagher\\PycharmProjects\\CSC-440-Project\\content"
-UPLOAD_DIR = "C:\\Users\\Justin Gallagher\\PycharmProjects\\CSC-440-Project\\wiki\\web\\static\\upload"
-HISTORY_SHOW_MAX = 30
-NUMBER_OF_HISTORY = 5
 FILE_SIZE_MAX_MB = 16
+SECRET_KEY= "a unique and long key"
+CONTENT_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\content"
+UPLOAD_DIR = "C:\\Users\\ongew\\Desktop\\Transfer\\CSC 440\\Projects\\PyKi\\wiki\\web\\static\\upload"
 CONNECTION_STRING = "DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE=PyKi.db;Trusted_connection=yes"
